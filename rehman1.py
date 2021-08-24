dd5bfb974931283a64d181debf8cf80ae03014ad
BilaLDogar19 <89441766+BilaLDogar19@users.noreply.github.com>
2021-08-24 11:29:03 +0500

    Update rehman1.py

--- rehman1.py
+++ rehman1.py
@@ -209,8 +209,8 @@ jalan("\033[1;96m
 jalan('\033[1;93m              Welcome to Dogar HACKER GROUP
 print "\033[1;97m•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•\033[1;96mBALOCHHACKER\033[1;97m•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•"
 
-CorrectUsername = "junaidjamshed"
-CorrectPassword = "lovetariqjameel"
+CorrectUsername = "BILAL DOGAR"
+CorrectPassword = "BILALKING"
 
 loop = 'true'
 while (loop == 'true'):
@@ -524,7 +524,7 @@ def pilih_super():
 	p.map(main, id)
 	print "\033[1;96m•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•\033[1;93mDogarHACKER\033[1;96m•◈•▬ ▬ ▬ ▬ ▬ ▬ ▬•◈•"
 	print "  \033[1;93m«---•◈•---Developed By Dogar-Hacker--•◈•---»" #Dev:Baloch_hacker
-	print '\033[1;96m✅Process Has Been Completed Press➡ Ctrl+Z.↩ Next Type (python2 Shabirbaloch.py)↩\033[1;97m....'
+	print '\033[1;96m✅Process Has Been Completed Press➡ Ctrl+Z.↩ Next Type (Python2 rehman1. py)↩\033[1;97m....'
 	print"\033[1;92mTotal OK/\x1b[1;93mCP \033[1;93m: \033[1;97m"+str(len(oks))+"\033[1;97m/\033[1;93m"+str(len(cekpoint))
 	print """
                               
